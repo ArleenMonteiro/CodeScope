9a401e9ef5078cd92b03d5ae6cad575d86fed6f2
Arleen Monteiro <111902870+ArleenMonteiro@users.noreply.github.com>
2024-02-25 02:14:13 -0500

    Create index.js

---
+++ src/components/configurations/index.js
@@ -0,0 +1,4 @@
+export { default as bedrockConfig } from './bedrock';
+export { default as openaiConfig } from './openai';
+export { default as pgrevucLangchainConfig } from './pg-revuc-langchain';
+export { default as pgrevucConfig } from './pg-revuc';
